72728aeaae38d594aadf2274d3f52c50f0839c11
DJ_Kaosun <djkaosun@gmail.com>
2021-01-17 12:58:37 +0900

    コメントの追加および不要ファイルの削除。

--- DesktopClockLibrary/CustomHoliday.cs
+++ DesktopClockLibrary/CustomHoliday.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 
 namespace DesktopClock.Library
 {
+    /// <summary>
+    /// カスタム休日。
+    /// </summary>
     public class CustomHoliday : ICustomHoliday
     {
         private ObservableCollection<KeyValuePair<DateTime, string>> _Holidays;
+        /// <summary>
+        /// カスタム休日の一覧。
+        /// 1 回しかセットできません。2 回以上セットしようとすると、<see cref="InvalidOperationException"/> がスローされます。
+        /// </summary>
         public ObservableCollection<KeyValuePair<DateTime, string>> Holidays
         {
             get { return _Holidays; }
@@ -21,6 +28,13 @@ namespace DesktopClock.Library
             }
         }
 
+        /// <summary>
+        /// 年月日に対応するカスタム休日の名前を取得します。
+        /// </summary>
+        /// <param name="year">年。</param>
+        /// <param name="month">月。</param>
+        /// <param name="day">日。</param>
+        /// <returns>カスタム休日名。</returns>
         public string GetHolidayName(int year, int month, int day)
         {
             if (Holidays == null) return null;
