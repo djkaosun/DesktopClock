7fd0d6dcf1a3dbdbbff67e4458057cb45f2eaf2f
DJ_Kaosun <djkaosun@gmail.com>
2021-01-20 12:27:05 +0900

    ユニットテスト用のクラス追加。
    FakeDateTimeEventSource。

---
+++ DesktopClockTests/FakeDateTimeEventSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesktopClock.Library;
+
+namespace DesktopClockTests
+{
+    public class FakeDateTimeEventSource : IDateTimeEventSource
+    {
+    }
+}
